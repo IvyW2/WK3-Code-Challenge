834f65ee81f6c7d766f6568a09e32e54528ce653
IvyW2 <ivywambugha2@gmail.com>
2022-12-12 21:58:13 +0300

    Flatdango

---
+++ README.md
@@ -0,0 +1,58 @@
+# Flatdango
+
+## Description
+- This is an application that allows a user to purchase movie tickets from the theater.
+
+## Live Link
+
+https://ivy-wk2.netlify.app/
+
+## User Story
+- A user can see a display of all movies available in the theater.
+- A user can select a particular movie that he/she wants to purchase tickets from the theater.
+- A User can buy tickets of different movies unless sold out upon which an alert will pop up.
+
+
+## Project Setup:
+This is a code challenge that has been handled using Javascript only.
+In order for you to use the content on this repo ensure you have the following:
+- A computer running on either Windows 7+, Linux or Mac OS.
+
+## Development prerequisites
+- Visual Studio Code.
+- Live Server Visual Studio Extension
+
+## Installation
+1. Open a terminal/ comand line interphase on your computer
+2. cloning the repo using the following command: 
+```bash
+        git clone git@github.com:IvyW2/WK3-Code-Challenge.git
+```
+3. Move to the folder using:
+```bash
+        cd WK3-Code-Challenge
+```      
+4. Open it in `Visual Studio Code` by using the command:
+```bash
+        code .
+```
+## Running application
+
+- To run the application use the following command in the terminal
+```bash
+        json-server --watch db.json
+```
+- After the server runs right click on the on the HTML file './index.html' on the primary side bar to open the context menu and select **Open with Live Server** to launch it in a browser. 
+- Test the code functionality left click on a movie on the left which will be displayed on the right and input the number of tickets desired by clicking on '+' button to reach the desired number of tickets. Click on the Buy Tickets button for confirmation of sold tickets. User can also click on another movie to buy tickets as well. User can use the same instructions to navigate through the other movies on the list. Once tickets are sold out user will get an alert and will not be able to purchase any more tickets for that particular movie.
+
+
+## Technology used
+- HTML
+- Javascript
+
+## Author
+Project contributed to by:
+- [Ivy Wambugha](https://github.com/IvyW2/)
+
+## Licence
+ISC licence
---
+++ src/films.js
@@ -0,0 +1,140 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetchMovies();
+  });
+  
+  const BASE_URL = `http://localhost:3000/`
+
+  const fetchMovies = () => {
+    fetch(BASE_URL + "films")
+      .then((resp) => resp.json())
+      .then(populateMovieList);
+  }
+  const populateMovieList = (films) => {
+    displayFilms(films)
+    const buyTicketBtn = document.querySelector("#buy-ticket");
+    buyTicketBtn.addEventListener("click", () => {
+        films = films.map(film => {
+            if(film.tickets_bought && film.tickets_sold <= film.capacity){
+                film.tickets_sold = parseInt(film.tickets_sold) + parseInt(film.tickets_bought)
+                displayFilm(film)
+                film.tickets_bought=0
+            } 
+            return film
+        });
+        const filmTickets = document.querySelector("#ticket-count");
+        filmTickets.value = 0;
+      });
+  }
+
+  const displayFilms = (films) =>{
+    films.map((film) => {
+        setFilmDetails(film)
+      });
+  }
+  //Displaying current film selected
+  let currentFilm; 
+  const setFilmDetails = (film) =>{
+    const movies = document.querySelector("#movies-list");
+    const moviesList = document.createElement("li");
+    moviesList.classList.add('col-6', 'text-center', 'py-2')
+
+    const movieListCard = document.createElement('div')
+    movieListCard.classList.add('card', 'mx-2')
+
+    const movieListHeading = document.createElement('h2')
+    movieListHeading.classList.add('mt-3')
+    movieListHeading.textContent = film.title
+
+    const movieListImage = document.createElement("img");
+    movieListImage.classList.add('mx-auto', 'my-2')
+    movieListImage.src = film.poster;
+
+    const movieListShowTime = document.createElement('b')
+    movieListShowTime.classList.add('d-block')
+    movieListShowTime.textContent = 'ShowTime: '+film.showtime
+
+    const movieListDesc = document.createElement('p')
+    movieListDesc.classList.add('d-block')
+    movieListDesc.textContent = film.description
+    
+    movieListCard.appendChild(movieListHeading)
+    movieListCard.appendChild(movieListImage)
+    movieListCard.appendChild(movieListShowTime)
+    movieListCard.appendChild(movieListDesc)
+    
+    moviesList.appendChild(movieListCard);
+    movies.appendChild(moviesList);
+    
+    movies.style.cursor = "pointer";
+
+    moviesList.addEventListener("click", () => {
+      currentFilm = film;
+      displayFilm(film);
+    });
+  }
+  const displayFilm = (film) => {
+    const title = document.querySelector("#title");
+    title.innerHTML = film.title;
+    const filmImg = document.querySelector("img#display-image");
+    filmImg.src = film.poster;
+    const filmDesc = document.querySelector("#description");
+    filmDesc.textContent = film.description;
+    const showTime = document.querySelector("#showTime");
+    showTime.textContent = 'ShowTime '+film.showtime;
+    const capacity = document.querySelector("#capacity");
+    capacity.textContent = 'capacity '+film.capacity;
+    const titcketsSold = document.querySelector("#sold");
+    titcketsSold.textContent = 'Tickets Sold : ' +film.tickets_sold;
+    const titcketsRemaining = document.querySelector("#remaining");
+    const ticketsRemaining = film.capacity - film.tickets_sold
+    if(ticketsRemaining===0){
+        titcketsRemaining.textContent = 'Sold Out ';
+    }else{
+        titcketsRemaining.textContent = 'Tickets Remaining : ' +(film.capacity - film.tickets_sold);
+    }
+    const runTime = document.querySelector("#runTime");
+    let hrs =  Math.floor(film.runtime/60)
+    let minutes = film.runtime%60
+    runTime.textContent = 'Duration: ' +hrs + ' hrs ' + minutes +' minutes';
+    const filmTickets = document.querySelector("#ticket-count");
+    filmTickets.value = currentFilm.tickets_bought ?? 0;
+  }
+  
+  //Adding votes for the selected Character
+  const addTicket = () => {
+    if(currentFilm == null){
+      return alert('You have not selected a film')
+      
+    }
+    const filmTickets = document.querySelector("#ticket-count");
+    const boughtTickets = parseInt(filmTickets.value)
+    
+    if(currentFilm.tickets_sold + boughtTickets >= currentFilm.capacity){
+      return alert("Tickets exceed available capacity")
+        
+    }
+    
+    if(currentFilm.tickets_bought){
+        currentFilm.tickets_bought += 1
+    }else{
+        currentFilm.tickets_bought = 1
+    }
+    filmTickets.value = currentFilm.tickets_bought ?? 0;
+    
+  }
+
+  const removeTicket = () => {
+    if(currentFilm == null){
+        alert('You have not selected a film')
+    }
+    if(currentFilm.tickets_bought){
+        currentFilm.tickets_bought -= 1
+    }else{
+        currentFilm.tickets_bought = 0
+    }
+    
+    const filmTickets = document.querySelector("#ticket-count");
+    filmTickets.value = currentFilm.tickets_bought ?? 0;
+  }
+
+
